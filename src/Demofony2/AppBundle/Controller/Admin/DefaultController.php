88e5615ca304cfe14c5e6e9d0db1247ff4bfeeb5
Marc Morales Valldepérez <marcmorales83@gmail.com>
2014-11-14 16:06:52 +0100

    added admin template

---
+++ src/Demofony2/AppBundle/Controller/Admin/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Demofony2\AppBundle\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/admin/dashboard/", name="demofony2_admin_dashboard")
+     */
+    public function dashboardAction()
+    {
+        return $this->render(':Admin/Default:dashboard.html.twig');
+    }
+}
