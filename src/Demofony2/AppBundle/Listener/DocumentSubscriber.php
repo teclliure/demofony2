09835b033da596b123cf0d93b3a136c1b88d0366
Marc Morales Valldepérez <marcmorales83@gmail.com>
2014-12-10 18:08:29 +0100

    api get image url

---
+++ src/Demofony2/AppBundle/Listener/DocumentSubscriber.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Demofony2\AppBundle\Listener;
+
+use Demofony2\AppBundle\Entity\Comment;
+use Demofony2\AppBundle\Entity\Document;
+use Demofony2\AppBundle\Manager\FileManager;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Events;
+
+class DocumentSubscriber implements EventSubscriber
+{
+    protected $fileManager;
+
+    public function __construct(FileManager $fm)
+    {
+        $this->fileManager = $fm;
+    }
+
+    public function getSubscribedEvents()
+    {
+        return array(
+            Events::postLoad,
+        );
+    }
+
+    /**
+     * @param LifecycleEventArgs $args
+     */
+    public function postLoad(LifecycleEventArgs $args)
+    {
+        $object = $args->getEntity();
+
+        if ($object instanceof Document) {
+            $url = $this->fileManager->getDocumentUrl($object);
+            $object->setUrl($url);
+        }
+    }
+}
