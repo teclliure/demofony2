544f6b19d844f4ac39d81aa16d1214395d6ebe79
David Romaní <david@flux.cat>
2014-12-11 16:44:43 +0100

    split front controller complexity

---
+++ src/Demofony2/AppBundle/Controller/Front/ParticipationController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Demofony2\AppBundle\Controller\Front;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * Class ParticipationController
+ *
+ * @category Controller
+ * @package  Demofony2\AppBundle\Controller\Front
+ * @author   David Romaní <david@flux.cat>
+ */
+class ParticipationController extends Controller
+{
+    /**
+     * @Route("/participation/", name="demofony2_front_participation")
+     */
+    public function participationAction()
+    {
+        return $this->render('Front/participation.html.twig');
+    }
+
+    /**
+     * @Route("/participation/calendar/", name="demofony2_front_participation_calendar")
+     */
+    public function participationCalendarAction()
+    {
+        return $this->render('Front/participation/calendar.html.twig');
+    }
+
+    /**
+     * @Route("/participation/discussions/", name="demofony2_front_participation_discussions")
+     */
+    public function participationDiscussionsAction()
+    {
+        return $this->render('Front/participation/discussions.html.twig');
+    }
+
+    /**
+     * @Route("/participation/discussions/{id}/{discussion}/", name="demofony2_front_participation_discussions_edit")
+     */
+    public function participationDiscussionsEditAction($id, $discussion)
+    {
+        return $this->render('Front/participation/discussions.edit.html.twig', array('discussion' => $discussion));
+    }
+
+    /**
+     * @Route("/participation/porposals/", name="demofony2_front_participation_proposals")
+     */
+    public function participationProposalsAction()
+    {
+        return $this->render('Front/participation/proposals.html.twig');
+    }
+
+    /**
+     * @Route("/participation/porposals/add-new-proposal/", name="demofony2_front_participation_proposals_new")
+     */
+    public function participationProposalsNewAction()
+    {
+        return $this->render('Front/participation/proposals.new.html.twig');
+    }
+}
