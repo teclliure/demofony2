211afa9852e4735d21771639d8ef461cc00b4002
David Romaní <david@flux.cat>
2014-12-13 20:12:06 +0100

    add process participation repository features

--- src/Demofony2/AppBundle/Repository/ProcessParticipationRepository.php
+++ src/Demofony2/AppBundle/Repository/ProcessParticipationRepository.php
@@ -36,8 +36,37 @@ class ProcessParticipationRepository extends BaseRepository
         $now = new \DateTime();
 
         return $this->createQueryBuilder('p')
-//            ->where('p.finishedAt > :today')
+            ->where('p.finishAt > :today')
             ->andWhere('p.presentationAt > :today')
+            ->setParameter('today', $now->format('Y-m-d H:i:s'))
+            ->orderBy('p.createdAt', 'DESC')
+            ->setMaxResults($n)
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * Get 10 last close discussions
+     *
+     * @return ArrayCollection
+     */
+    public function get10LastCloseDiscussions()
+    {
+        return $this->getNLastCloseDiscussions(self::MAX_LISTS_ITEMS);
+    }
+
+    /**
+     * Get n last close discussions
+     *
+     * @param int $n
+     * @return ArrayCollection
+     */
+    public function getNLastCloseDiscussions($n = self::MAX_LISTS_ITEMS)
+    {
+        $now = new \DateTime();
+
+        return $this->createQueryBuilder('p')
+            ->where('p.finishAt <= :today')
             ->setParameter('today', $now->format('Y-m-d H:i:s'))
             ->orderBy('p.createdAt', 'DESC')
             ->setMaxResults($n)
