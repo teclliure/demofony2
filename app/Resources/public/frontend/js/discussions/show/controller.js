33cb710a38b4d36153fdd4d6c6cdbb1aeac012d3
David Romaní <david@flux.cat>
2014-12-13 10:44:57 +0100

    add ng resources

---
+++ app/Resources/public/frontend/js/discussions/show/controller.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('discussionShowApp')
+    .controller('MainCtrl', ['CFG', 'uiGmapGoogleMapApi', '$scope', '$timeout', '$routeParams', '$log', function (CFG, uiGmapGoogleMapApi, $scope, $timeout, $routeParams, $log) {
+
+        $scope.discussion = angular.fromJson(asyncDiscussion);
+
+        $scope.map = {
+            center: { latitude: 41, longitude: 0 },
+            zoom: 4,
+            bounds: {},
+            clusterOptions: { gridSize: 80, maxZoom: 20, averageCenter: true, minimumClusterSize: 1, zoomOnClick: false }
+        };
+        $scope.map.options = {
+            scrollwheel: true,
+            draggable: true,
+            maxZoom: 15
+        };
+        $scope.map.control = {};
+
+        uiGmapGoogleMapApi.then(function(maps) {
+            // promise done
+            $log.log('uiGmapGoogleMapApi loaded', maps);
+        });
+
+    }]);
