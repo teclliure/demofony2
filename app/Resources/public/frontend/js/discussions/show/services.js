da2c0b376d89904199e032b87fa58953c3a67fc4
David Romaní <david@flux.cat>
2014-12-14 12:33:54 +0100

    fix JS lint error in ng services.js

--- app/Resources/public/frontend/js/discussions/show/services.js
+++ app/Resources/public/frontend/js/discussions/show/services.js
@@ -1,10 +1,12 @@
+'use strict';
+
 var services = angular.module('discussionShowApp.services', []);
 
 services.factory('Security', function ($q) {
     return {
         canVoteInProcessParticipation: function(state, is_logged) {
               return $q(function(resolve, reject) {
-                if (state == 2 && is_logged) {
+                if (state === 2 && is_logged) {
                     resolve();
                 } else {
                     reject('Vote is not open');
