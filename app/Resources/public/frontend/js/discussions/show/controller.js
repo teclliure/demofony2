0ad40bb828f3ab6140e2700f80f05200967c3f4b
Marc Morales Valldepérez <marcmorales83@gmail.com>
2015-01-15 15:51:36 +0100

    show modal login to vote when not login

--- app/Resources/public/frontend/js/discussions/show/controller.js
+++ app/Resources/public/frontend/js/discussions/show/controller.js
@@ -43,28 +43,32 @@ angular.module('discussionShowApp').controller('MainCtrl', ['CFG', 'uiGmapGoogle
                 $scope.discussion.total_votes_count--;
             });
         }, function() {
-            //failed
+             $scope.showModal.login();
         });
     };
 
     $scope.comment = {
         like: function(comment, index) {
-            console.log('entra 123');
-            $scope.canVotePromise.then(function() {
-                var url = Routing.generate('api_post_processparticipation_comments_like', { id: $scope.discussion.id, comment_id: comment.id });
-                //substring is to resolve a bug between routing.generate and restangular
-                var like = Restangular.all(url.substring(1));
-                if (!comment.user_already_like) {
-                    like.post().then(function(result) {
-                        $scope.comments.comments[index] = result;
-                    });
+             $scope.canVotePromise.then(function() {
+                 var url = Routing.generate('api_post_processparticipation_comments_like', {
+                     id: $scope.discussion.id,
+                     comment_id: comment.id
+                 });
+                 //substring is to resolve a bug between routing.generate and restangular
+                 var like = Restangular.all(url.substring(1));
+                 if (!comment.user_already_like) {
+                     like.post().then(function (result) {
+                         $scope.comments.comments[index] = result;
+                     });
 
-                    return;
-                }
-                like.remove().then(function(result) {
-                    $scope.comments.comments[index] = result;
-                });
-            });
+                     return;
+                 }
+                 like.remove().then(function (result) {
+                     $scope.comments.comments[index] = result;
+                 });
+             }, function() {
+                 $scope.showModal.login();
+             });
         },
         unlike: function(comment, index) {
             $scope.canVotePromise.then(function() {
@@ -81,6 +85,8 @@ angular.module('discussionShowApp').controller('MainCtrl', ['CFG', 'uiGmapGoogle
                 like.remove().then(function(result) {
                     $scope.comments.comments[index] = result;
                 });
+            }, function() {
+                $scope.showModal.login();
             });
         },
         post: function (commentTosend, parent) {
@@ -88,10 +94,11 @@ angular.module('discussionShowApp').controller('MainCtrl', ['CFG', 'uiGmapGoogle
             $scope.canVotePromise.then(function() {
                 var url = Routing.generate('api_post_processparticipation_comments', { id: $scope.discussion.id});
                 var comment = Restangular.all(url.substring(1));
-                console.log(commentTosend);
                 comment.post(commentTosend).then(function(result) {
                     $scope.comments.comments.push(result);
                 });
+            }, function() {
+                $scope.showModal.login();
             });
         },
         put: function (commentTosend) {
@@ -102,10 +109,20 @@ angular.module('discussionShowApp').controller('MainCtrl', ['CFG', 'uiGmapGoogle
                 comment.customPUT(tosend).then(function(result) {
                     $('#edit-comment-' + commentTosend.id).addClass('hide');
                 });
+            }, function() {
+                $scope.showModal.login();
             });
         },
         showEditForm: function (id) {
             $('#edit-comment-' + id).removeClass('hide');
+        }
+    };
+
+    $scope.showModal = {
+        login: function() {
+            if (!$scope.is_logged) {
+                jQuery('#login-modal-form').modal({show: true});
+            }
         }
     };
 
