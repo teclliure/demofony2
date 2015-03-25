ba54a22241303acbb08adf5bc0dccf60526a6666
Marc Morales Valldepérez <marcmorales83@gmail.com>
2015-03-25 18:37:55 +0100

    bus edit user profile

--- src/Demofony2/UserBundle/Form/Type/ProfileFormType.php
+++ src/Demofony2/UserBundle/Form/Type/ProfileFormType.php
@@ -21,13 +21,32 @@ class ProfileFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $myEntity = $builder->getForm()->getData();
+
         $builder
             ->add('username', null, array())
             ->add('email', 'email', array('read_only' => true, 'disabled' => true))
             ->add('name', 'text', array())
             ->add('description', 'textarea', array())
-            ->add('image', 'file', array('required' => false))
-            ->add('newsletterSubscribed', 'checkbox', array('required' => false))
+//            ->add('image', 'comur_image', array(
+//                'label' => 'image',
+//                'required' => false,
+//                'uploadConfig' => array(
+//                    'uploadRoute' => 'comur_api_upload',        //optional
+//                    'uploadUrl' => $myEntity->getUploadRootDir(),       // required - see explanation below (you can also put just a dir path)
+//                    'webDir' => $myEntity->getUploadDir(),              // required - see explanation below (you can also put just a dir path)
+//                    'fileExt' => '*.jpg;*.gif;*.png;*.jpeg',    //optional
+//                    'libraryDir' => null,                       //optional
+//                    'showLibrary' => false,                      //optional
+//                ),
+//                'cropConfig' => array(
+//                    'minWidth' => 100,
+//                    'minHeight' => 100,
+//                    'aspectRatio' => true,              //optional
+//                    'forceResize' => false,             //optional        )
+//                )
+//            ))
+        ->add('newsletterSubscribed', 'checkbox', array('required' => false))
             ->add('gps', new GpsFormType(), array())
 //            ->add('current_password', 'password', array(
 //                'mapped' => false,
