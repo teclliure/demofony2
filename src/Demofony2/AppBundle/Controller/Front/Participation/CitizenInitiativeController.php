481c287fa1e392bbea161873a138699deb1e54e5
Marc Morales Valldepérez <marcmorales83@gmail.com>
2015-04-17 11:55:50 +0200

    citizen initiatives detail 1

--- src/Demofony2/AppBundle/Controller/Front/Participation/CitizenInitiativeController.php
+++ src/Demofony2/AppBundle/Controller/Front/Participation/CitizenInitiativeController.php
@@ -2,10 +2,12 @@
 
 namespace Demofony2\AppBundle\Controller\Front\Participation;
 
+use Demofony2\AppBundle\Entity\CitizenInitiative;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 /**
  * CitizenInitiativeController
@@ -15,12 +17,12 @@ class CitizenInitiativeController extends Controller
     const ITEMS_BY_PAGE = 5;
 
     /**
-     * @Route("/participation/citizen-initiative/open{open}/", name="demofony2_front_participation_citizen_initiative_list_open")
-     * @Route("/participation/citizen-initiative/closed{closed}/", name="demofony2_front_participation_citizen_initiative_list_closed")
-     *
      * @param Request $request
      * @param int     $open
      * @param int     $closed
+
+     * @Route("/participation/citizen-initiative/open{open}/", name="demofony2_front_participation_citizen_initiative_list_open")
+     * @Route("/participation/citizen-initiative/closed{closed}/", name="demofony2_front_participation_citizen_initiative_list_closed")
      *
      * @return Response
      */
@@ -41,6 +43,24 @@ class CitizenInitiativeController extends Controller
             'openInitiatives' => $openInitiatives,
             'closedInitiatives' => $closedInitiatives,
             'open' => $isOpenTab,
+        ));
+    }
+
+    /**
+     * @param Request $request
+     * @param CitizenInitiative $initiative
+     *
+     * @Route("/participation/citizen-initiative/{id}/", name="demofony2_front_participation_citizen_initiative_detail")
+     * @ParamConverter("initiative", class="Demofony2AppBundle:CitizenInitiative")
+     *
+     * @return Response
+     */
+    public function detailAction(Request $request, CitizenInitiative $initiative)
+    {
+
+
+        return $this->render(':Front/participation:citizen-initiatives-detail.html.twig', array(
+            'initiative' => $initiative,
         ));
     }
 }
