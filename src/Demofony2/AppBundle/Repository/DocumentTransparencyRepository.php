349e0b77022963d413491c16dba41cb35b625314
David Romaní <david@flux.cat>
2015-10-23 10:10:47 +0200

    fix https://trello.com/c/lhHKlJLo/108-recents-transparencia

--- src/Demofony2/AppBundle/Repository/DocumentTransparencyRepository.php
+++ src/Demofony2/AppBundle/Repository/DocumentTransparencyRepository.php
@@ -4,6 +4,13 @@ namespace Demofony2\AppBundle\Repository;
 
 class DocumentTransparencyRepository extends BaseRepository
 {
+    /**
+     * Get more interesting items sorted by visits amount
+     *
+     * @param int $max
+     *
+     * @return array
+     */
     public function getMoreInteresting($max = 5)
     {
         $qb = $this->createQueryBuilder('d');
@@ -11,6 +18,25 @@ class DocumentTransparencyRepository extends BaseRepository
         $qb->select('d, c')
             ->leftJoin('d.category', 'c')
             ->orderBy('d.visits', 'DESC')
+            ->setMaxResults($max);
+
+        return $qb->getQuery()->getResult();
+    }
+
+    /**
+     * Get last items sorted by date
+     *
+     * @param int $max
+     *
+     * @return array
+     */
+    public function getLastItems($max = 5)
+    {
+        $qb = $this->createQueryBuilder('d');
+
+        $qb->select('d, c')
+            ->leftJoin('d.category', 'c')
+            ->orderBy('d.updatedAt', 'DESC')
             ->setMaxResults($max);
 
         return $qb->getQuery()->getResult();
